3cc04dbcfc0d544db92cda9340348509f18f787f
Rohit Raj <88172539+rohitrajknoldus@users.noreply.github.com>
2021-12-30 00:14:08 +0530

    After fixing checkstyle

--- src/main/java/com/spring/services/CourseServiceImpl.java
+++ src/main/java/com/spring/services/CourseServiceImpl.java
@@ -2,33 +2,33 @@ package com.spring.services;
 
 import com.SpringWithController.entity.Courses;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-class CourseServiceImpl implements CoursesService{
-
-    List<Courses> list;
-    public CourseServiceImpl()
-    {
-        list=new ArrayList<>();
-        list.add(new Courses(1618,"Java Course","This is Java Assignment with controller and API"));
-
+class CourseServiceImpl implements CoursesService {
+    /*
+    private list created
+     */
+    private List<Courses> list;
+    /*
+    constructor
+     */
+    CourseServiceImpl() {
+        list = new ArrayList<>();
+        list.add(new Courses(1618, "Java Course",
+                "This is Java Assignment with controller and API"));
     }
-
     @Override
-    public List<Courses> getCourses(long l) {
+    public List<Courses> getCourses(final long l) {
         return null;
     }
-
     @Override
-    public Course getCourse(long courseId) {
+    public Course getCourse(final long courseId) {
         return null;
     }
-
     @Override
-    public <Course> Course getCourse(String courseId) {
+    public <Course> Course getCourse(final String courseId) {
         return null;
     }
 }
