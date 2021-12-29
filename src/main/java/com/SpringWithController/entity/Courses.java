793d76a92302f159708c715edc494eb663ad0ac4
Rohit Raj <88172539+rohitrajknoldus@users.noreply.github.com>
2021-12-30 01:29:29 +0530

    After fixing Checkstyle

--- src/main/java/com/SpringWithController/entity/Courses.java
+++ src/main/java/com/SpringWithController/entity/Courses.java
@@ -4,47 +4,43 @@ public class Courses {
     private long id;
     private  String title;
     private String description;
-
-    public Courses(long id, String title, String description) {
+/*
+* Parametrized Constructor
+ */
+    public Courses(final long id, final String title,
+                   final String description) {
         this.id = id;
         this.title = title;
         this.description = description;
     }
-
+    /*
+     * constructor
+     */
     public Courses() {
-        super ();
+        super();
     }
-
     public long getId() {
         return id;
     }
-
-    public void setId(long id) {
+    public void setId(final long id) {
         this.id = id;
     }
-
     public String getTitle() {
         return title;
     }
-
-    public void setTitle(String title) {
+    public void setTitle(final String title) {
         this.title = title;
     }
-
     public String getDescription() {
         return description;
     }
-
-    public void setDescription(String description) {
+    public void setDescription(final String description) {
         this.description = description;
     }
-
     @Override
     public String toString() {
-        return "Courses{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                '}';
+        return "Courses{" + "id=" + id
+                + ", title='" + title + '\''
+                + ", description='" + description + '\'' + '}';
     }
 }
